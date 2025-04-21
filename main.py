59310fec36345085f71a6d937703bfc79ad1b57b
KikiHanAKKH <ayekhantig@gmail.com>
2025-04-21 13:05:05 -0700

    merged

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import character
-import shield
-
+from character import Character
+from bard import Bard
+from warrior import Warrior
+from wizard import Wizard
+from decorator import Decorator
+from shield import Shield
+from cloak import Cloak
+from ring import Ring
 
 def main():
 
