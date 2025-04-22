5ba0afc6c421af9a66530b15915cef1aef596663
Isaac <isaacyunkim@gmail.com>
2025-04-22 10:44:19 -0700

    added .gitignore, added comment to character.py

--- character.py
+++ character.py
@@ -2,6 +2,10 @@ import abc
 
 class Character(abc.ABC): 
 
+    """
+    Abstract interface for character object. 
+    """
+    
     @abc.abstractmethod
     def description(self): 
         """Returns a description of the character."""
