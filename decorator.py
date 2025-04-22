345f4bce3328af99b3e8fe50ee1a416216045670
Isaac <isaacyunkim@gmail.com>
2025-04-22 10:50:57 -0700

    add decorator.py comments

--- decorator.py
+++ decorator.py
@@ -1,17 +1,26 @@
 from character import Character
 
+"""
+Decorator class for character objects.
+"""
+
 class Decorator(Character):
     def __init__(self, c):
+        # Decorator constructor
         self._char = c
 
     def description(self):
+        """Returns a description of the character."""
         return self._char.description()
     
     def magic_resistance(self):
+        """Returns the character's magic resistance."""
         return self._char.magic_resistance()
     
     def strength(self):
+        """Returns the character's attack power."""
         return self._char.strength()
     
     def __str__(self):
+        """Returns a string representation of the character."""
         return super().__str__()
